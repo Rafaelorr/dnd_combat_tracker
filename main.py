cac3b5915a17a7bdb72295923f88c697d91e7795
Rafaelorr <rafael.onkelinx@muscardinus.be>
2025-11-15 11:51:39 +0100

    voeg command skelet toe aan main.py

---
+++ main.py
@@ -0,0 +1,27 @@
+from functies import *
+invaties :dict = {}
+
+if __name__ == "__main__":
+    print("DnD combat tracker")
+    print("Typ 'help' voor een lijst van alle commands")
+    while True:
+        command = input(": ")
+        if command.lower() == "help":
+            help()
+        elif command.lower() == "exit":
+            exit()
+        elif command.lower() == "list":
+            print(invaties)
+        elif command.lower() == "add":
+            add(invaties)
+        elif command.lower() == "delete":
+            pass
+        elif command.lower() == "show":
+            pass
+        elif command.lower() == "edit_hp":
+            pass
+        elif command.lower() == "edit_ac":
+            pass
+        else:
+            print("Invalid command")
+            print("Typ 'help' voor een lijst van alle commands")
